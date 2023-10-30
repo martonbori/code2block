61082abb01760166ec2488a7533c801a6b30328c
Bori Márton <50830608+martonbori@users.noreply.github.com>
2023-10-30 09:12:07 +0100

    Separated main.py from app.py

--- src/code2block/classes/app.py
+++ src/code2block/classes/app.py
@@ -64,7 +64,6 @@ class App:
     def document_symbols(self) -> List[SymbolInformation] | List[DocumentSymbol] | None:
         doc_id = TextDocumentIdentifier(uri=self.document.uri)
         document_symbols = self.lsp_client.documentSymbol(text_document=doc_id)
-        print(f"document_symbols:\n{document_symbols}\n")
         return document_symbols.result
 
     def signature_help(self, module_name, method) -> SignatureHelp:
@@ -82,7 +81,6 @@ class App:
             position=Position(line=1, character=index+1)
         )
         signatures = self.lsp_client.signatureHelp(text_document_position)
-        print(f"signatures:\n{signatures}\n")
         return signatures
 
     def completion_request(self, line, character) -> List[CompletionItem]:
@@ -92,7 +90,6 @@ class App:
         )
         completion_event = self.lsp_client.completion(text_document_position)
         completion_list = completion_event.completion_list.items
-        print(f"completion_list:\n{completion_list}\n")
         return completion_list
 
     def find_symbols_rec(self, root_symbol, found_symbols=None) -> List[str]:
@@ -124,43 +121,20 @@ class App:
         self.document.text = f"import {module_name}"
         self.update_file()
         completion_list = self.find_symbols(module_name)
+        print(f"Found {len(completion_list)} symbols")
         signature_data = {}
-        for completion_item in completion_list:
+        for idx, completion_item in enumerate(completion_list):
             label = completion_item.label
+            print(f"Get signature data [{idx+1}/{len(completion_list)}] ({label})")
             signatures = self.signature_help(module_name, label).signatures
             if not signatures:
                 continue
             signature_data[label] = signatures
 
-        blocks_data = block_factory.generate_blocks(module_name=module_name,
+        return block_factory.generate_blocks(module_name=module_name,
                                       completion_list=completion_list,
                                       signature_data=signature_data)
 
-        with open(r"D:\Egyetem\2023_tavasz\Szakdolgozat\repos\project\blocks.js", "w") as block_file:
-            block_file.write("block_definitions = ")
-            block_file.write(json.dumps(blocks_data["blocks"], default=lambda o: o.__dict__))
-        with open(r"D:\Egyetem\2023_tavasz\Szakdolgozat\repos\project\toolbox_data.js", "w") as toolbox_file:
-            toolbox_file.write("toolbox_data = ")
-            toolbox_file.write(json.dumps(blocks_data["toolbox_category"], default=lambda o: o.__dict__))
-        with open(r"D:\Egyetem\2023_tavasz\Szakdolgozat\repos\project\code_generators.js", "w") as generators_file:
-            generators_text = ""
-            for generator in blocks_data["code_generators"]:
-                generators_text += "\nBlockly.Python['" + generator[
-                    'block_type'] + "'] = function(block) {\n\tvar code = '" + generator[
-                                       'code'] + "';\n\treturn code;\n};\n"
-            generators_file.write(generators_text)
-
-        return
 
     def exit(self):
         self.lsp_client.exit()
-
-
-if __name__ == '__main__':
-    pylsp_command = ["pylsp", "-v", "--log-file", r"D:\Egyetem\2023_tavasz\Szakdolgozat\Logs\pylsp.log"]
-    app = App(pylsp_command)
-    app.open_file("D:/Egyetem/2023_tavasz/Szakdolgozat/Workspace/test.py")
-
-    test_module = input("Give me a module name: ")
-    app.generate_blocks(test_module)
-    app.exit()
