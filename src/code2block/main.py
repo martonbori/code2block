aa6fa256101255a230084b06f66818669d17b2e3
Bori Márton <50830608+martonbori@users.noreply.github.com>
2023-11-13 17:01:52 +0100

    Changed log file paths

--- src/code2block/main.py
+++ src/code2block/main.py
@@ -5,9 +5,9 @@ from src.code2block.classes.app import App
 from src.code2block.gui import webserver
 
 if __name__ == '__main__':
-    pylsp_command = ["pylsp", "-v", "--log-file", r"D:\Egyetem\2023_tavasz\Szakdolgozat\Logs\pylsp.log"]
+    pylsp_command = ["pylsp", "-v", "--log-file", r"C:/Users/Marci/Documents/Egyetem/2023_tavasz/Szakdolgozat/Logs/pylsp.log"]
     app = App(pylsp_command)
-    app.open_file("D:/Egyetem/2023_tavasz/Szakdolgozat/Workspace/test.py")
+    app.open_file("C:/Users/Marci/Documents/Egyetem/2023_tavasz/Szakdolgozat/Workspace/test.py")
 
     webserver.start_webserver(app)
     app.exit()
