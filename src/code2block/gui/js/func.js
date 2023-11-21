471af8d46b7c93ae2fb371d7555380761e57eae5
Bori Márton <50830608+martonbori@users.noreply.github.com>
2023-11-21 07:46:11 +0100

    Add test block to BlockMirror text to block converter

--- src/code2block/gui/js/func.js
+++ src/code2block/gui/js/func.js
@@ -40,32 +40,255 @@ function init_editor() {
         "toolboxitemid": "import_module_btn",
         "disabled": "True"
     }
-    add_category(null, add_category_button, null, -1)
+    add_category(null, null,add_category_button, -1)
     
     $("#import_module_btn").on("click", function() {
         console.log("click")
         add_module_modal = $("#import_module_modal")
         add_module_modal.modal("show")
     });
+
+
+    
+    
+
+    Blockly.Extensions.registerMutator(
+        "test_mutator",
+        {
+            "saveExtraState": function() {
+                return {
+                  'testAttrib': "test atrib value",
+                };
+              },
+              
+            "loadExtraState": function(state) {
+                this.testAttrib = state['testAttrib'];
+                // This is a helper function which adds or removes inputs from the block.
+                this.updateShape_();
+            },
+            "mutationToDom": function() {
+                var container = Blockly.utils.xml.createElement('mutation');
+                container.setAttribute('testAttrib', "test atrib value");
+                return container;
+            },
+            "domToMutation": function() {
+                this.testAttrib = xmlElement.getAttribute('testAttrib');
+                this.updateShape_();
+            }
+        }
+    )
+    toolbox = Blockly.getMainWorkspace().options.languageTree
+    test_block_xml = '<block type="ast_Call" line_number="1" inline="true"><mutation arguments="1" returns="false" parameters="true" method="false" name="turtle.forward" message="move turtle forward by" premessage="" colour="140" module="turtle"><arg name="UNKNOWN_ARG:0"></arg></mutation><value name="ARG0"><block type="ast_Num" line_number="1"><field name="NUM">50</field></block></value></block>'
+    console.log(toolbox)
+    
+    BlockMirrorTextToBlocks.prototype.MODULE_FUNCTION_SIGNATURES['json'] = {}
+    BlockMirrorTextToBlocks.prototype.MODULE_FUNCTION_SIGNATURES['json']['load'] = {
+        "returns": true,
+        "simple": ['fp','cls','object_hook','parse_float', 'parse_int', 'parse_constant', 'object_pairs_hook','kwds'],
+        "message": "json.load",
+        "colour": BlockMirrorTextToBlocks.COLOR.PLOTTING
+    };
+    block_xml = getFunctionBlock('load',{},'json')
+    
+    test_category = {
+        "kind": "category",
+        "name": "Test",
+        "contents": [{kind:"block", type:"ast_Call", blockxml:Blockly.utils.xml.textToDom(block_xml)}]
+    }
+    //console.log(test_block_json)
+    add_category("json", [{"name":"load", "args": ['a','b']}])
+
 }
 
-function add_category(blocks, category, code_generators, idx=-2) {
+function add_category(module_name, blocks, category_json, idx=-2) {
     /** Add blocks to Workspace **/
-    if(blocks) {
-        Blockly.defineBlocksWithJsonArray(blocks);
-        Object.assign(Blockly.Blocks, blocks)    
+    if (!category_json) {
+        category_json  = {
+            "kind": "category",
+            "name": "Test",
+        }
     }
-
+    if (blocks) {
+        BlockMirrorTextToBlocks.prototype.MODULE_FUNCTION_SIGNATURES[module_name] = {}
+        category_contents = []
+        blocks.forEach(block => {
+            block_name = block["name"]
+            BlockMirrorTextToBlocks.prototype.MODULE_FUNCTION_SIGNATURES[module_name][block_name] = {
+                "returns": true,
+                "simple": block["args"],
+                "message": module_name + "." + block_name,
+                "colour": BlockMirrorTextToBlocks.COLOR.PLOTTING
+            };
+            block_xml = getFunctionBlock(block_name,{},module_name)
+            category_contents.push({kind:"block", type:"ast_Call", blockxml:Blockly.utils.xml.textToDom(block_xml)})
+        });    
+        category_json["contents"] = category_contents
+    }
     /** Update Toolbox **/
     toolbox = Blockly.getMainWorkspace().options.languageTree
     toolbox_json = Blockly.utils.toolbox.convertToolboxDefToJson(toolbox)
     if (idx < 0) {
         idx = toolbox_json["contents"].length + 1 + idx
     }
-    toolbox_json["contents"].splice(idx,0, category)
+    toolbox_json["contents"].splice(idx,0, category_json)
     Blockly.getMainWorkspace().updateToolbox(toolbox_json);
 }
 
+
+function getFunctionBlock(name, values, module) {
+    if (values === undefined) {
+        values = {};
+    }
+    // TODO: hack, we shouldn't be accessing the prototype like this
+    let signature;
+    let method = false;
+    if (module !== undefined) {
+        signature = BlockMirrorTextToBlocks.prototype.MODULE_FUNCTION_SIGNATURES[module][name];
+    } else if (name.startsWith('.')) {
+        signature = BlockMirrorTextToBlocks.prototype.METHOD_SIGNATURES[name.substr(1)];
+        method = true;
+    } else {
+        signature = BlockMirrorTextToBlocks.prototype.FUNCTION_SIGNATURES[name];
+    }
+    let args = (signature.simple !== undefined ? signature.simple :
+               signature.full !== undefined ? signature.full : []);
+    let argumentsMutation = {
+        "@arguments": args.length,
+        "@returns": (signature.returns || false),
+        "@parameters": true,
+        "@method": method,
+        "@name": module ? module+"."+name : name,
+        "@message": signature.message ? signature.message : name,
+        "@premessage": signature.premessage ? signature.premessage : "",
+        "@colour": signature.colour ? signature.colour : 0,
+        "@module": module || ""
+    };
+    for (let i = 0; i < args.length; i += 1) {
+        argumentsMutation["UNKNOWN_ARG:" + i] = null;
+    }
+    let newBlock = BlockMirrorTextToBlocks.create_block("ast_Call", null, {},
+        values, {inline: true}, argumentsMutation);
+    // Return as either statement or expression
+    return BlockMirrorTextToBlocks.xmlToString(newBlock);
+};
+
+function create_block_json(type, lineNumber, fields, values, settings, mutations, statements) {
+    var newBlock = {
+        "type": type,
+        "line_number": lineNumber
+    }
+    
+    for (var setting in settings) {
+        var settingValue = settings[setting];
+        newBlock[setting] = settingValue;
+    }
+    // Mutations
+    /*
+    if (mutations !== undefined && Object.keys(mutations).length > 0) {
+        mutator_functions = {
+            decompose: function(workspace) {
+                // This is a special sub-block that only gets created in the mutator UI.
+                // It acts as our "top block"
+                var topBlock = workspace.newBlock('lists_create_with_container');
+                topBlock.initSvg();
+              
+                // Then we add one sub-block for each item in the list.
+                var connection = topBlock.getInput('STACK').connection;
+                for (var i = 0; i < this.itemCount_; i++) {
+                  var itemBlock = workspace.newBlock('lists_create_with_item');
+                  itemBlock.initSvg();
+                  connection.connect(itemBlock.previousConnection);
+                  connection = itemBlock.nextConnection;
+                }
+              
+                // And finally we have to return the top-block.
+                return topBlock;
+              },
+        }
+        args = {}
+        for (let mutation in mutations) {
+            var mutationValue = mutations[mutation];
+            if (mutation.charAt(0) === '@') {
+                newMutation[mutation.substr(1)] = mutationValue;
+            } else if (mutationValue != null && mutationValue.constructor === Array) {
+                for (var i = 0; i < mutationValue.length; i++) {
+                    let mutationNode = {};
+                    mutationNode["name"] = mutationValue[i];
+                    newMutation.appendChild(mutationNode);
+                }
+            } else {
+                let mutationNode = document.createElement("arg");
+                if (mutation.charAt(0) === '!') {
+                    mutationNode.setAttribute("name", "");
+                } else {
+                    mutationNode.setAttribute("name", mutation);
+                }
+                if (mutationValue !== null) {
+                    mutationNode.appendChild(mutationValue);
+                }
+                newMutation.appendChild(mutationNode);
+            }
+        }
+        Blockly.Extensions.registerMutator(
+            type + '_mutator',
+            {
+                saveExtraState: function() {
+                    return {
+                      'itemCount': this.itemCount_,
+                    };
+                  },
+                  
+                  loadExtraState: function(state) {
+                    this.itemCount_ = state['itemCount'];
+                    // This is a helper function which adds or removes inputs from the block.
+                    this.updateShape_();
+                  },
+            },
+            undefined,
+            []
+        );
+
+        newBlock.appendChild(newMutation);
+    }*/
+    // Fields
+    for (var field in fields) {
+        var fieldValue = fields[field];
+        var newField = document.createElement("field");
+        newField.setAttribute("name", field);
+        newField.appendChild(document.createTextNode(fieldValue));
+        newBlock.appendChild(newField);
+    }
+    // Values
+    for (var value in values) {
+        var valueValue = values[value];
+        var newValue = document.createElement("value");
+        if (valueValue !== null) {
+            newValue.setAttribute("name", value);
+            newValue.appendChild(valueValue);
+            newBlock.appendChild(newValue);
+        }
+    }
+    // Statements
+    if (statements !== undefined && Object.keys(statements).length > 0) {
+        for (var statement in statements) {
+            var statementValue = statements[statement];
+            if (statementValue == null) {
+                continue;
+            } else {
+                for (var i = 0; i < statementValue.length; i += 1) {
+                    // In most cases, you really shouldn't ever have more than
+                    //  one statement in this list. I'm not sure Blockly likes
+                    //  that.
+                    var newStatement = document.createElement("statement");
+                    newStatement.setAttribute("name", statement);
+                    newStatement.appendChild(statementValue[i]);
+                    newBlock.appendChild(newStatement);
+                }
+            }
+        }
+    }
+    return newBlock;
+}
 
 /******* JQuery functions *******/
 
